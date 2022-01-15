5f513a50d9d1561657cad7ecf3462321f78e772d
Vishal Kumar <vishal.kumar@benchwork.in>
2022-01-15 15:09:26 +0530

    minor fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -25,7 +25,7 @@ function Home() {
         <Card
           src="https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
           title="Entire homes"
-          description="Comfortable private places, with room for friends & family"
+          description="Comfortable private places, with room for friends & family "
         />
         <Card
           src="https://cdn.pixabay.com/photo/2016/11/21/16/21/bed-1846251__340.jpg"
